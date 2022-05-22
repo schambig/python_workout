c79c6795d5e15e41bf0c12fc49a473b40984112d
Salomon Chambi <salomonchambi@gmail.com>
2022-05-22 12:01:00 -0500

    feat: use print function, comment in a single and multiple lines

---
+++ fundamentals/01-print.py
@@ -0,0 +1,22 @@
+print("Hola, esto es el archivo 01")
+
+print("Hola soy Grogu y tengo 1 año")
+
+# operaciones...
+
+print(12 + 5)
+print(10 - 5)
+print(2 * 3)
+print(8 / 2)
+
+# Esto es un comentario de una linea
+
+"""
+varias
+lineas
+otra
+"""
+'''
+varias 
+lienas
+'''
