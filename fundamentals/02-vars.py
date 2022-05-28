1598c142f993f62049d63ec579da4abc235a9c0a
Salomon Chambi <salomonchambi@gmail.com>
2022-05-28 12:01:00 -0500

    feat: use variables and input function

---
+++ fundamentals/02-vars.py
@@ -0,0 +1,18 @@
+print("Hola, vars")
+
+# esto es una variable
+my_name = "Grogu"
+print(my_name)
+
+# esto es variable con un numero
+my_age = 1
+print(my_age)
+
+my_name = "Salomon"
+print("aqui cambio", my_name)
+
+# usando la funcion input
+my_name = input("Cual es tu nombre?")
+print("usando input", my_name)
+
+# For more info check this link: https://www.w3schools.com/python/python_variables.asp
