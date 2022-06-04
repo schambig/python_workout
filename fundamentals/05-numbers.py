c7c504e49a4d2fcc1d306fa93bfb6e9a96cbdc5e
Salomon Chambi <salomonchambi@gmail.com>
2022-06-04 12:03:00 -0500

    feat: usgin integers and scientific notation

---
+++ fundamentals/05-numbers.py
@@ -0,0 +1,38 @@
+lives = 3
+print(type(lives))
+age = 12
+budget = 100
+
+temperature = 12.12
+print(type(temperature))
+
+lives = 2
+print(lives)
+lives = 1
+print(lives)
+
+lives = 12 + 15
+print(lives)
+
+lives = lives - 1
+print(lives)
+
+lives -= 1
+print(lives)
+
+lives -= 5
+print(lives)
+
+lives += 5
+print(lives)
+
+
+# Usando notacion cientifica
+
+# Con numero muy grande
+number = 4500000000000000000.1
+print(number) # Output: 4.5e+18
+
+# Con numero muy pequeño
+number_b = 0.0000000000000001
+print(number_b) # Output: 1e-16
