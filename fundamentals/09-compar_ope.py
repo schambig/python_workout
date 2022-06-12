d5b62eeae6c2cad47be0ff07ddf7d44b4c8405f9
Salomon Chambi <salomonchambi@gmail.com>
2022-06-12 12:05:00 -0500

    feat: use comparison operators

---
+++ fundamentals/09-compar_ope.py
@@ -0,0 +1,49 @@
+# > 
+print('>')
+print(7 > 3)
+print(3 > 7)
+print(7 > 7)
+print()
+
+# <
+print('<')
+print(5 < 6)
+print(6 < 5)
+print(5 < 5)
+print()
+
+# >=
+print('>=')
+print(2 >= 1)
+print(2 >= 3)
+print(2 >= 2)
+print()
+
+# <=
+print('<=')
+print(1 <= 2)
+print(2 <= 1)
+print(2 <= 2)
+print()
+
+# ==
+print('==')
+print(6 == 6)
+print(5 == 2)
+print()
+
+# !=
+print('!=')
+print(6 != 10)
+print(6 != 6)
+print()
+
+# Usando operadores de comparacion con strings
+print('Comparison op con strings')
+print("Apple" == 'Apple')
+print("Apple" == 'apple')
+print("1" == 1)
+print()
+
+age = 15
+print(age >= 18)
