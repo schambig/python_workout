ca2481ef17616570b3f68b94fb748105491d5072
Salomon Chambi <salomonchambi@gmail.com>
2022-06-12 12:04:00 -0500

    feat: use arithmetic operators

---
+++ fundamentals/08-aritm_ope.py
@@ -0,0 +1,24 @@
+print(10 + 10)
+print(10 - 5)
+print(10 * 2)
+
+# Even if one integer devides another exactly the result will be a float
+print(10 / 2) # Output: 5.0
+print(10 % 2) # Output: 0
+print(10 / 3) # Output: 3.3333333333333335
+print(10 % 3)
+
+# Floor division: devides and rounds down to the nearest int
+print(10 // 3) # Output: 3
+print(2 ** 3)
+
+# Python follows PEMDAS, and first execute normal division then floor division
+print(2 ** 3 + 3 - 7 / 1 // 4) # Output: 10.0
+print(2 ** 3)
+print((7 / 1) // 4)
+
+print(8 + 3 - 1)
+# print(10 / 0) Output: Traceback... ZeroDivisionError: division by zero
+
+print('Hola' + ' mundo')
+print('Hola' * 3)
