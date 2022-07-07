433ee28e4de0d7348c039dc42d6415871f1bc3ea
Salomon Chambi <salomonchambi@gmail.com>
2022-07-07 12:05:00 -0500

    feat: add more conditionals to rock_paper_scissors game

--- fundamentals/main.py
+++ fundamentals/main.py
@@ -1,5 +1,12 @@
 user_option = input('piedra, papel o tijera => ')
-computer_option = 'piedra'
+computer_option = 'tijera'
 
 if user_option == computer_option:
     print('Empate!')
+elif user_option == 'piedra':
+    if computer_option == 'tijera':
+        print('piedra gana a tijera')
+        print('gana el usuario!')
+    else:
+        print('papel gana a piedra')
+        print('gana la compu!')
