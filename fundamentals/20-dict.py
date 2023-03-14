c82e5dd1ddc450bd3d07d621ae6698077b5cabca
Salomon Chambi <salomonchambi@gmail.com>
2023-03-14 12:28:00 -0500

    feat: add the output for different methods

--- fundamentals/20-dict.py
+++ fundamentals/20-dict.py
@@ -8,7 +8,7 @@ person = {
 }
 
 print('Dicionario person:')
-print(person)
+print(person) # Output: {'name': 'salo', 'last_name': 'chambi', 'langs': ['python', 'javascript'], 'age': 99}
 print()
 
 print('Actualizando valores del diccionario:')
@@ -16,22 +16,22 @@ print('Actualizando valores del diccionario:')
 person['name'] = 'luis'
 person['age'] -= 50
 person['langs'].append('rust')
-print(person)
+print(person) # Output: {'name': 'luis', 'last_name': 'chambi', 'langs': ['python', 'javascript', 'rust'], 'age': 49}
 print()
 
 # Eliminando del diccionario usando keys
 print('Eliminando valores del diccionario:')
 del person['last_name']
 person.pop('age')
-print('person =>', person)
+print(person) # Output: {'name': 'luis', 'langs': ['python', 'javascript', 'rust']}
 print()
 
 
 print('Metodo .items() :')
-print(person.items())
+print(person.items()) # Output: dict_items([('name', 'luis'), ('langs', ['python', 'javascript', 'rust'])])
 
 print('Metodo .keys() :')
-print(person.keys())
+print(person.keys()) # Output: dict_keys(['name', 'langs'])
 
 print('Metodo .values() :')
-print(person.values())
+print(person.values()) # Output: dict_values(['luis', ['python', 'javascript', 'rust']])
