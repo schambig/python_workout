e6a35579ff0ec6006e347da61de4e13c26df4336
Salomon Chambi <salomonchambi@gmail.com>
2023-03-15 12:29:00 -0500

    feat: add loops' output as comments

--- fundamentals/22-for.py
+++ fundamentals/22-for.py
@@ -5,15 +5,25 @@ for element in range(1, 21): # 1: inclusive, 21: exclusive
 '''
 
 my_list = [23, 45, 67, 89 ,43]
-print('Lista my_list:', my_list)
+print(my_list) # Output: [23, 45, 67, 89, 43]
 for element in my_list:
   print(element)
+# Output:
+# 23
+# 45
+# 67
+# 89
+# 43 
 print()
 
 my_tuple = ('salo', 'juli', 'santi')
-print('Tupla my_tuple:', my_tuple)
+print(my_tuple) # Output: ('salo', 'juli', 'santi')
 for element in my_tuple:
   print(element)
+# Output: 
+# salo
+# juli
+# santi
 print()
 
 product = {
@@ -21,13 +31,20 @@ product = {
   'price': 100,
   'stock': 89
 }
-print('Diccionario Producto:', product)
+print(product) # Output: {'name': 'Camisa', 'price': 100, 'stock': 89}
 for key in product:
   print(key, '=>', product[key])
-
-for key, value in product.items(): # Retorna un array con key:value
+# Output: 
+# name => Camisa
+# price => 100
+# stock => 89
+for key, value in product.items():
   print(key, '=>', value)
-print('product.items():', product.items())
+# Output: 
+# name => Camisa
+# price => 100
+# stock => 89
+print(product.items()) # Output: dict_items([('name', 'Camisa'), ('price', 100), ('stock', 89)])
 print()
 
 people = [
@@ -47,7 +64,13 @@ people = [
 
 for person in people:
   print('name =>', person['name'])
-  # print(person)
+  # Output:
+  # name => grogu
+  # name => din
+  # name => bo-katan
+
+for person in people:
+  print(person)
   # Output:
   #   {'name': 'grogu', 'age': 150}         
   #   {'name': 'din', 'age': 45}
