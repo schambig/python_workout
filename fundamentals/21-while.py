77277868ad18fb5bb7514be5f5ca4a159c529327
Salomon Chambi <salomonchambi@gmail.com>
2023-03-15 12:28:00 -0500

    feat: add the output for loops as comments

--- fundamentals/21-while.py
+++ fundamentals/21-while.py
@@ -6,6 +6,7 @@ counter = 0
 while counter < 10:
   counter += 1
   print(counter)
+'''
 
 counter = 0
 while counter < 20:
@@ -13,7 +14,23 @@ while counter < 20:
   if counter == 15:
     break # Termina el programa 
   print(counter)
-'''
+   # Output:
+   # 1
+   # 2
+   # 3
+   # 4
+   # 5
+   # 6
+   # 7
+   # 8
+   # 9
+   # 10
+   # 11
+   # 12
+   # 13
+   # 14
+
+print()
 
 counter = 0
 while counter < 20:
@@ -21,3 +38,11 @@ while counter < 20:
   if counter < 15:
     continue # Salta automaticamente a la siguiente iteracion sin ejecutar el codigo que sigue
   print(counter)
+  # Output:
+  # 15
+  # 16
+  # 17
+  # 18
+  # 19
+  # 20
+ 
