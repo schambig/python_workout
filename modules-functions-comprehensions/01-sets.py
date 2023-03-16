11cec6603533caaec3ec44b2385ed0d6f9d30044
Salomon Chambi <salomonchambi@gmail.com>
2023-03-16 12:33:00 -0500

    fix: include set description and its external link

--- modules-functions-comprehensions/01-sets.py
+++ modules-functions-comprehensions/01-sets.py
@@ -19,3 +19,6 @@ set_numbers = set(numbers) # cast to set (no duplicates)
 print(set_numbers) # output: {1, 2, 3, 4} 
 unique_numbers = list(set_numbers) # cast to list
 print(unique_numbers) # output: [1, 2, 3, 4]
+
+# Set is one of 4 built-in data types in Python used to store collections of data, the other 3 are List, Tuple, and Dictionary, all with different qualities and usage.
+# More infor about sets: https://www.w3schools.com/python/python_sets.asp
