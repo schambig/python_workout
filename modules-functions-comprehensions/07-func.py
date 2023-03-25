57f7d5dda9548db9a49ee252a796156c663582d4
Salomon Chambi <salomonchambi@gmail.com>
2023-03-25 12:33:00 -0500

    feat: explore the basic of functions

---
+++ modules-functions-comprehensions/07-func.py
@@ -0,0 +1,21 @@
+print('Hola') # Hola
+
+def my_print(text):
+  print(text * 2)
+
+my_print('Este es my texto ') # Este es my texto Este es my texto
+my_print('Hola ') # Hola Hola
+
+a = 10
+b = 90
+
+c = a + b
+
+def suma(a, b):
+  my_print(a + b) # We are using a function inside another
+
+suma(1, 5) # 6 * 2 = 12
+suma(10, 4) # 14 * 2 = 28
+  
+# More info about functions:
+# https://www.w3schools.com/python/python_functions.asp
