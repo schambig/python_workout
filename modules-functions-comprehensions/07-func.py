0e148783ab826865ddfc95719eaa70a2b9dc12ba
Salomon Chambi <salomonchambi@gmail.com>
2023-03-26 12:33:00 -0500

    fix: refactor output of function

--- modules-functions-comprehensions/07-func.py
+++ modules-functions-comprehensions/07-func.py
@@ -3,7 +3,7 @@ print('Hola') # Hola
 def my_print(text):
   print(text * 2)
 
-my_print('Este es my texto ') # Este es my texto Este es my texto
+my_print('Este es my texto ') # Este es mi texto Este es mi texto
 my_print('Hola ') # Hola Hola
 
 a = 10
@@ -14,8 +14,8 @@ c = a + b
 def suma(a, b):
   my_print(a + b) # We are using a function inside another
 
-suma(1, 5) # 6 * 2 = 12
-suma(10, 4) # 14 * 2 = 28
+suma(1, 5) # (1 + 5) * 2 = 12
+suma(10, 4) # (10 + 4) * 2 = 28
   
 # More info about functions:
 # https://www.w3schools.com/python/python_functions.asp
