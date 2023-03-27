4bbbe6d05b14ba24110f811354b4f912461ca62c
Salomon Chambi <salomonchambi@gmail.com>
2023-03-27 12:33:00 -0500

    fix: add more examples in comments

--- modules-functions-comprehensions/07-func.py
+++ modules-functions-comprehensions/07-func.py
@@ -19,3 +19,26 @@ suma(10, 4) # (10 + 4) * 2 = 28
   
 # More info about functions:
 # https://www.w3schools.com/python/python_functions.asp
+
+# Arbitrary Arguments, *args
+#   def my_function(*kids):
+#   print("The youngest child is " + kids[2])
+
+#   my_function("Emil", "Tobias", "Linus")
+
+# Arbitrary Keyword Arguments, **kwargs
+#   def my_function(**kid):
+#     print("His last name is " + kid["lname"])
+
+#   my_function(fname = "Tobias", lname = "Refsnes")
+
+# Default Parameter Value
+#   def my_function(country = "Norway"):
+#     print("I am from " + country)
+
+#   my_function("Sweden")
+#   my_function("India")
+#   my_function()
+#   my_function("Brazil")
+
+# and more...
