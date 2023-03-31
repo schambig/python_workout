49afa530da3fb41d54e65deebfe36b20814834ff
Salomon Chambi <salomonchambi@gmail.com>
2023-03-31 12:33:00 -0500

    fix: practice with multiple return values

--- modules-functions-comprehensions/09-multiple-return.py
+++ modules-functions-comprehensions/09-multiple-return.py
@@ -1,10 +1,8 @@
 def get_volume(length=1, width=2, depth=3):
     return length * width * depth, width, 'Salo'
 
-res = get_volume(width=7)
 vol, len, text = get_volume(width=7)
 
-print(res)
 print(vol)
 print(len)
 print(text)
