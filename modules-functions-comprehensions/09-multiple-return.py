fed360b6cb35e53e35ab4291970a1fbaeb1e2255
Salomon Chambi <salomonchambi@gmail.com>
2023-04-01 12:33:00 -0500

    feat: add outputs as comments

--- modules-functions-comprehensions/09-multiple-return.py
+++ modules-functions-comprehensions/09-multiple-return.py
@@ -1,8 +1,10 @@
 def get_volume(length=1, width=2, depth=3):
     return length * width * depth, width, 'Salo'
 
-vol, len, text = get_volume(width=7)
+# res = get_volume(width=7) # This will return a tuple
+vol, width, text = get_volume(width=7)
 
-print(vol)
-print(len)
-print(text)
+# print(res) # (21, 7, 'Salo')
+print(vol) # 21
+print(width) # 7
+print(text) # Salo
