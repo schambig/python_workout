8ef5d443b052326851ae3b74cc8f868f7a65be90
Salomon Chambi <salomonchambi@gmail.com>
2023-04-02 12:33:00 -0500

    feat: add more outputs as comments

--- modules-functions-comprehensions/09-multiple-return.py
+++ modules-functions-comprehensions/09-multiple-return.py
@@ -5,6 +5,9 @@ def get_volume(length=1, width=2, depth=3):
 vol, width, text = get_volume(width=7)
 
 # print(res) # (21, 7, 'Salo')
+# print(res[0]) # 21
+# print(res[1]) # 7
+# print(res[2]) # Salo
 print(vol) # 21
 print(width) # 7
 print(text) # Salo
