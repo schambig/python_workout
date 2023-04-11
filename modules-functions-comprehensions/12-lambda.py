32213df030c9bdae5edd5cc40912e1627c9746ca
Salomon Chambi <salomonchambi@gmail.com>
2023-04-11 12:33:00 -0500

    feat: practice with lambda function

---
+++ modules-functions-comprehensions/12-lambda.py
@@ -0,0 +1,20 @@
+def increment(x):
+  return x + 1
+
+
+increment_v2 = lambda x: x + 1
+
+result = increment(10)
+print(result)
+
+result = increment_v2(20)
+print(result)
+
+full_name = lambda name, last_name: f'Full name is {name.title()} {last_name.title()}'
+
+text = full_name('salomon', 'chambi gutierrez')
+print(text)
+
+
+# More about Lamdba functions:
+# https://www.w3schools.com/python/python_lambda.asp
