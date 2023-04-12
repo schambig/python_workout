b08f473e5b89452c55664fccc376528f10144eef
Salomon Chambi <salomonchambi@gmail.com>
2023-04-12 12:33:00 -0500

    feat: get fibonacci series using lambda and recursion

--- modules-functions-comprehensions/12-lambda.py
+++ modules-functions-comprehensions/12-lambda.py
@@ -1,7 +1,6 @@
 def increment(x):
   return x + 1
 
-
 increment_v2 = lambda x: x + 1
 
 result = increment(10)
@@ -15,6 +14,10 @@ full_name = lambda name, last_name: f'Full name is {name.title()} {last_name.tit
 text = full_name('salomon', 'chambi gutierrez')
 print(text)
 
+# Fibonacci series using lambda function and recursion:
+
+# fibonacci = lambda x: 0 if x == 0 else 1 if x < 3 else fibonacci(x - 1) + fibonacci(x - 2)
+# print(fibonacci(int(input('Enter a number: '))))
 
 # More about Lamdba functions:
 # https://www.w3schools.com/python/python_lambda.asp
