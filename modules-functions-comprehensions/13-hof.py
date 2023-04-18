69f6dad7784a1986c84288e7727e1c50ab1ef82c
Salomon Chambi <salomonchambi@gmail.com>
2023-04-18 12:33:00 -0500

    fix: correct comment on using hof with another functions

--- modules-functions-comprehensions/13-hof.py
+++ modules-functions-comprehensions/13-hof.py
@@ -1,4 +1,4 @@
-# Using Regular Functions
+# Using HOF with Regular Functions
 def increment(x):
   return x + 1
 def high_ord_func(x, func):
@@ -8,14 +8,14 @@ res = high_ord_func(3, increment)
 # 3 + (3 + 1)
 print(res)
 
-# Using Higher Order Functions
+# Using HOF with Lambda Functions
 increment_v2 = lambda x: x +1
 high_ord_func_v2 = lambda x, func: x + func(x)
 
 result = high_ord_func_v2(2, increment_v2)
 print(result)
 
-# We can jus pass a lambda function as argument
+# We can just pass a lambda function as argument
 result = high_ord_func_v2(2, lambda x: x + 2)
 print(result)
 ## change
@@ -23,7 +23,7 @@ result = high_ord_func_v2(2, lambda x: x * 5)
 print(result)
 
 
-# In Python, a higher-order function is a function that takes one or more functions as arguments, or returns a function as its result.
+# In Python, a higher-order function (HOF) is a function that takes one or more functions as arguments, or returns a function as its result.
 # Higher-order functions are often used to make code more reusable and concise.
 
 # Some examples of higher-order functions in Python include:
