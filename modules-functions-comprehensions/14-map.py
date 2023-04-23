e9740807a5726822e130350daf7e584ab944c7b8
Salomon Chambi <salomonchambi@gmail.com>
2023-04-23 12:33:00 -0500

    feat: add descriptive comments and source link

--- modules-functions-comprehensions/14-map.py
+++ modules-functions-comprehensions/14-map.py
@@ -1,8 +1,10 @@
+# Using the regular approach
 numbers = [1, 2, 3, 4]
 numbers_v2 = []
 for i in numbers:
   numbers_v2.append(i * 2)
 
+# Using map and lambda function (one liner)
 numbers_v3 = list(map(lambda i: i * 2, numbers))
 
 print(numbers)
@@ -16,3 +18,6 @@ print(numbers_1)
 print(numbers_2)
 result = list(map(lambda x, y: x + y, numbers_1, numbers_2))
 print(result)
+
+# More about amp function:
+# https://www.w3schools.com/python/ref_func_map.asp
