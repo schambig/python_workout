438cfe0633732b8f2523563f99b258418057f2c1
Salomon Chambi <salomonchambi@gmail.com>
2023-05-05 12:33:00 -0500

    feat: practice with filter higher order function

--- modules-functions-comprehensions/17-filter.py
+++ modules-functions-comprehensions/17-filter.py
@@ -1,4 +1,8 @@
 numbers = [1,2,3,4,5]
 new_numbers = list(filter(lambda x: x % 2 == 0, numbers))
-print(new_numbers)
-print(numbers)
+print(new_numbers) # [2, 4]
+print(numbers) # [1, 2, 3, 4, 5] filter doesn't change the original list
+
+
+# More info about filter function:
+# https://www.w3schools.com/python/ref_func_filter.asp
