4429de180a613ec1e2e601e22d9ac7a1ff3aa92e
Salomon Chambi <salomonchambi@gmail.com>
2023-05-11 12:33:00 -0500

    feat: use lambda with reduce, add explanation graphic

--- modules-functions-comprehensions/18-reduce.py
+++ modules-functions-comprehensions/18-reduce.py
@@ -11,7 +11,16 @@ def accum(counter, item):
 # result = functools.reduce(accum, numbers)
 result = reduce(accum, numbers)
 print(result)
+# counter =>  1
+# item =>  2
+# counter =>  3
+# item =>  3
+# counter =>  6
+# item =>  4
+# 10
+# Look at this pic to better understand the print above:
+# https://drive.google.com/file/d/1A3LatTN-BrCrgmn-uATTthBG3iD_vG-G/view?usp=sharing 
 
 # Now let's create a lambda function instead of 'def accum()'
-res = reduce(lambda sum, item: sum + item, numbers)
-print(res)
+res = reduce(lambda count, item: count + item, numbers)
+print(res) # 10
