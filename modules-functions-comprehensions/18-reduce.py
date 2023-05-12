1ecff064334f797f91b3e64ddbba5c1f3dfe353f
Salomon Chambi <salomonchambi@gmail.com>
2023-05-12 12:33:00 -0500

    feat: add background operation as comment

--- modules-functions-comprehensions/18-reduce.py
+++ modules-functions-comprehensions/18-reduce.py
@@ -10,6 +10,9 @@ def accum(counter, item):
 
 # result = functools.reduce(accum, numbers)
 result = reduce(accum, numbers)
+# result = accum(accum(accum(accum(0, 1), 2), 3), 4) # this is what happens
+# Look at this pic to better understand:
+# https://drive.google.com/file/d/1A3LatTN-BrCrgmn-uATTthBG3iD_vG-G/view?usp=sharing 
 print(result)
 # counter =>  1
 # item =>  2
@@ -18,8 +21,6 @@ print(result)
 # counter =>  6
 # item =>  4
 # 10
-# Look at this pic to better understand the print above:
-# https://drive.google.com/file/d/1A3LatTN-BrCrgmn-uATTthBG3iD_vG-G/view?usp=sharing 
 
 # Now let's create a lambda function instead of 'def accum()'
 res = reduce(lambda count, item: count + item, numbers)
