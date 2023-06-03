e41f8f57541fa0c5b3ffcd7751f2c21064167961
Salomon Chambi <salomonchambi@gmail.com>
2023-06-03 12:33:00 -0500

    feat: create a namespace for this package

--- modules-functions-comprehensions/pkg/__init__.py
+++ modules-functions-comprehensions/pkg/__init__.py
@@ -1,5 +1,7 @@
 # This file will be executed just one time,
 # no matter how many modules its parent folder (the package) has.
+import pkg.mod_1, pkg.mod_2 # This will import the modules and its functions
+
 
 print('The package has been initialized')
 
