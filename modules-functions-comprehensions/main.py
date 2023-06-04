70acc4e8be26c4eeba0a2dbd14afa99a691f3d60
Salomon Chambi <salomonchambi@gmail.com>
2023-06-04 12:33:00 -0500

    feat: use namespace to import a module and its functions

--- modules-functions-comprehensions/main.py
+++ modules-functions-comprehensions/main.py
@@ -1,3 +1,4 @@
+'''
 # Import pkg # import everything from package pkg
 from pkg.mod_1 import func_1, func_2
 from pkg.mod_2 import func_3, func_4
@@ -7,7 +8,9 @@ print(func_1())
 print(func_2())
 print(func_3())
 print(func_4())
+'''
 
 # Import pkg to access URL variable created in __init__.py
 import pkg
 print(pkg.URL)
+print(pkg.mod_1.func_1()) # Use the imported modules in __init__.py using package namespace 
