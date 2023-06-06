da48939e3d83072ff0c11bdef6c207f0863f207c
Salomon Chambi <salomonchambi@gmail.com>
2023-06-06 12:33:00 -0500

    feat: iterate with for loop and manually

--- modules-functions-comprehensions/20-iterators.py
+++ modules-functions-comprehensions/20-iterators.py
@@ -1,6 +1,8 @@
+# Iterate using a for loop
 for i in range(1, 10):
   print(i)
 
+# Iterate manually using iter() and next() methods
 my_iter = iter(range(1, 4))
 print(my_iter)
 print(next(my_iter))
