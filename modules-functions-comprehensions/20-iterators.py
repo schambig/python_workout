1c12e44bc1943844697f6d74602853043800dc93
Salomon Chambi <salomonchambi@gmail.com>
2023-06-07 12:33:00 -0500

    feat: add print output as comments

--- modules-functions-comprehensions/20-iterators.py
+++ modules-functions-comprehensions/20-iterators.py
@@ -4,8 +4,11 @@ for i in range(1, 10):
 
 # Iterate manually using iter() and next() methods
 my_iter = iter(range(1, 4))
-print(my_iter)
+print(my_iter) # <range_iterator object at 0x7f79fa63a1b0>
 print(next(my_iter))
 print(next(my_iter))
 print(next(my_iter))
-print(next(my_iter))
+# print(next(my_iter)) # Traceback (most recent call last):
+#                          File "/home/salomon/githubRepos/python_workout/modules-functions-comprehensions/20-iterators.py", line 11, in <module>
+#                            print(next(my_iter))
+#                        StopIteration
