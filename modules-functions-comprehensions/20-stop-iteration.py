a53924ff34198332a83ae759de26efbb4c21d0c7
Salomon Chambi <salomonchambi@gmail.com>
2023-06-13 12:33:00 -0500

    feat: create an iterator and stop after 20 iterations

--- modules-functions-comprehensions/20-stop-iteration.py
+++ modules-functions-comprehensions/20-stop-iteration.py
@@ -1,8 +1,29 @@
-# First look `20-create-iterator.py`:
-# which would continue forever if you had enough next() statements, or if it was used in a for loop.
+'''
+This is `20-create-iterator.py`:
+which would continue forever if you had enough next() statements, or if it was used in a for loop.
 
-# To prevent the iteration from going on forever, we can use the StopIteration statement.
-# In the __next__() method, we can add a terminating condition to raise an error if the iteration is done a specified number of times:
+class MyNumbers:
+  def __iter__(self):
+    self.a = 1
+    return self
+
+  def __next__(self):
+    x = self.a
+    self.a += 1
+    return x
+
+myclass = MyNumbers()
+myiter = iter(myclass)
+
+print(next(myiter)) # 1
+print(next(myiter)) # 2
+print(next(myiter)) # 3
+print(next(myiter)) # 4
+print(next(myiter)) # 5
+
+To prevent the iteration from going on forever, we can use the StopIteration statement.
+In the __next__() method, we can add a terminating condition to raise an error if the iteration is done a specified number of times:
+'''
 
 class MyNumbers:
   def __iter__(self):
