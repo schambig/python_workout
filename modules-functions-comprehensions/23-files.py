03c68824311ab4f8da6d8f57feac1b6a68348bcd
Salomon Chambi <salomonchambi@gmail.com>
2023-06-21 12:33:00 -0500

    feat: read a txt file using 'with' keyword

--- modules-functions-comprehensions/23-files.py
+++ modules-functions-comprehensions/23-files.py
@@ -13,3 +13,25 @@ file.close()
 with open('./zenpy.txt') as file:
   for line in file:
     print(line)
+
+
+'''
+What does "with" do?
+
+"with" secretly inserts two method calls:
+
+with open(filename) as f:
+# f.__enter__()
+for one_line in f:
+print(one_line)
+# f.__exit__()
+
+In the case of a file, f.__exit__() flushes and closes the file,
+ensuring that all data is written (if appropriate) and that the file is closed.
+
+Yes, the file will be closed anyway when the Python program exits.
+But this guarantees that the file is closed at a known time.
+
+Any object that defines __enter__ and __exit__ is known as a context manager.
+Files are the most famous, but other objects (e.g., threading.Lock) also support them.
+'''
