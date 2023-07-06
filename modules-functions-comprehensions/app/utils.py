4320f326e40b5599f54b888b2b8cc85dbbbd1357
Salomon Chambi <salomonchambi@gmail.com>
2023-07-06 18:00:00 -0500

    feat: get a dictionary with year:population pair

--- modules-functions-comprehensions/app/utils.py
+++ modules-functions-comprehensions/app/utils.py
@@ -1,8 +1,17 @@
+import re
+
+
+def get_year_popu_from_csv(data):
+  year_popu = {key[:-11]: int(value) for key, value in data.items() if re.match('^[0-9]', key)}
+  keys = year_popu.keys()
+  values = year_popu.values()
+  return keys, values 
+
 def get_population():
   keys = ['per', 'col']
   values = [300, 400]
   return keys, values
 
 def population_by_country(data, country):
-  result = list(filter(lambda item: item['Country'] == country, data))
+  result = list(filter(lambda item: item['Country/Territory'] == country, data))
   return result
