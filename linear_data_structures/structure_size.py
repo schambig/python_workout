db1d39789e9c56ad78cda278db7804002657777b
Salomon Chambi <salomonchambi@gmail.com>
2023-07-11 08:00:00 -0500

    feat: get size of a structure

---
+++ linear_data_structures/structure_size.py
@@ -0,0 +1,7 @@
+import sys
+
+
+colecciones = {"list": list(), "tuple": tuple(), "dict": dict(), "set": set()}
+
+for name, value in colecciones.items():
+    print(f'{name} = {sys.getsizeof(value)} bytes')
