ba4369a9dad2dc2717b3a7cca297dafa6148b1de
Salomon Chambi <salomonchambi@gmail.com>
2023-07-12 08:00:00 -0500

    fix: get size of a structure

--- linear_data_structures/structure_size.py
+++ linear_data_structures/structure_size.py
@@ -1,7 +1,14 @@
 import sys
 
 
-colecciones = {"list": list(), "tuple": tuple(), "dict": dict(), "set": set()}
+colecciones = {
+    # mutables
+    "list": list(),
+    "dict": dict(),
+    "set": set(),
+    # mutables
+    "tuple": tuple(),
+    }
 
 for name, value in colecciones.items():
     print(f'{name} = {sys.getsizeof(value)} bytes')
