3fcc5c90caf3f58767abdc982ac926bc40872491
Salomon Chambi <salomonchambi@gmail.com>
2023-07-25 07:00:00 -0500

    feat: define variables

---
+++ fundamentals/1.introduction/0-vars.py
@@ -0,0 +1,9 @@
+course = 'Fundamental Python Practice'
+course = 'The value has changed'
+
+# Variable name should be descriptive a better option to above could be course_title
+# Should use snake_case
+
+full_name = 'Salomon Chambi'
+
+print(course)
