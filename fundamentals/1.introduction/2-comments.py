d355fccf4b40cf9fc4066fa0d3d41a16d83a5f18
Salomon Chambi <salomonchambi@gmail.com>
2023-07-28 07:00:00 -0500

    feat: comment your code

---
+++ fundamentals/1.introduction/2-comments.py
@@ -0,0 +1,12 @@
+# There are two type of comments (useful to leave annotations of our code) in Python:
+
+# 1. This is a one line comment
+
+"""2. This is a multiline comment
+We can follow different formats"""
+
+"""
+This is another way of using
+multiline comments.
+We can also use single quotes ''''Comment ''''
+"""
