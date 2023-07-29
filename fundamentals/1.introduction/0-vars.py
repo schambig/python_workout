df13632821679d236aa0eac865e48d7e7c5bf362
Salomon Chambi <salomonchambi@gmail.com>
2023-07-29 07:00:00 -0500

    feat: define variables in python

--- fundamentals/1.introduction/0-vars.py
+++ fundamentals/1.introduction/0-vars.py
@@ -1,8 +1,10 @@
 course = 'Fundamental Python Practice'
 course = 'The value has changed'
 
-# Variable name should be descriptive a better option to above could be course_title
-# Should use snake_case
+'''
+Variable name should be descriptive a better option to above could be course_title
+Should use snake_case
+'''
 
 full_name = 'Salomon Chambi'
 
