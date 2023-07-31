a26d3529f645f36abf3e30bbaa6c26c643f617b2
Salomon Chambi <salomonchambi@gmail.com>
2023-07-31 07:00:00 -0500

    feat: use some data types

---
+++ fundamentals/1.introduction/3-types.py
@@ -0,0 +1,21 @@
+# String, we could use single or double quotes
+course_title = 'Python Fundamental Practice'
+print(course_title)
+
+message = '''You are using a multiline
+message that will have new lines
+You can also use: \\n \n to make a new line'''
+print(message)
+
+# Integer
+first_number = 10
+print(first_number)
+
+# Float
+second_number = 3.1415
+print(second_number)
+
+# Boolean
+value = True
+value = False
+print(value)
