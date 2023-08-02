0c2c602f426ebeb89834af5d6412740e0d795909
Salomon Chambi <salomonchambi@gmail.com>
2023-08-02 07:00:00 -0500

    feat: compare variables using relational operators

---
+++ fundamentals/1.introduction/5-relational_ops.py
@@ -0,0 +1,24 @@
+'''
+Relational Operators (to compare), we get a Boolean value as result
+>, <, >=, <=, ==, !=
+'''
+first_number = 10
+second_number = 20
+
+result = first_number > second_number
+print(result)
+
+result = first_number < second_number
+print(result)
+
+result = first_number >= second_number
+print(result)
+
+result = first_number <= second_number
+print(result)
+
+result = first_number == second_number
+print(result)
+
+result = first_number != second_number
+print(result)
