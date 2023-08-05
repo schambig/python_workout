c614ba537d5cde2290fcc5913efdbe38f453cc4e
Salomon Chambi <salomonchambi@gmail.com>
2023-08-05 07:00:00 -0500

    feat: get the data type

---
+++ fundamentals/1.introduction/7-type.py
@@ -0,0 +1,15 @@
+'''
+To get the data type we use the type() function
+'''
+
+value = 'Salo'
+print(type(value))
+
+value = 2
+print(type(value))
+
+value = 3.1415
+print(type(value))
+
+value = True
+print(type(value))
