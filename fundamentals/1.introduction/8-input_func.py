934a27ab73ab1d1c2dccfaa31a34e296e7d438fb
Salomon Chambi <salomonchambi@gmail.com>
2023-08-07 07:00:00 -0500

    feat: get user info from standard input

--- fundamentals/1.introduction/8-input_func.py
+++ fundamentals/1.introduction/8-input_func.py
@@ -2,15 +2,15 @@
 We can use input() function to get info/data from the user (stdin)
 '''
 
-full_name = input('What is your full name: ') # str
+full_name = input('What is your full name: ') # str by default
 
-age = int(input('How old are you: '))
+age = int(input('How old are you: ')) # casting to integer
 
-height = float(input('How tall are you (in meters): '))
+height = float(input('How tall are you (in meters): ')) # casting to float
 
-auth = input('¿Do you authorize this program? (yes/no)') == 'yes'
+authorization = input('¿Do you authorize this program? (yes/no)') == 'yes'
 
 print(full_name)
 print(age)
 print(height)
-print(auth)
+print(authorization)
