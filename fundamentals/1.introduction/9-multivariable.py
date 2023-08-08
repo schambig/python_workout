6dd3fc76eaa2a839626a6cf0799e10eccec9b14c
Salomon Chambi <salomonchambi@gmail.com>
2023-08-08 07:00:00 -0500

    feat: create variables in one line

---
+++ fundamentals/1.introduction/9-multivariable.py
@@ -0,0 +1,14 @@
+'''
+We can create multiple variables in one line
+name = 'Salo'
+lastname = 'Chambi'
+title = 'Mr.'
+'''
+
+name, lastname, title = 'Salomon', 'Chambi', 'Mr.'
+
+print(name, lastname, title)
+print("*" * 50)
+print(name)
+print(lastname)
+print(title)
