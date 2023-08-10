137ec6bbf957d5bb358a0e893fd7d40390897794
Salomon Chambi <salomonchambi@gmail.com>
2023-08-10 07:00:00 -0500

    feat: define python lists

---
+++ fundamentals/2.lists/0-lists.py
@@ -0,0 +1,12 @@
+'''
+We have two options to create lists in Python
+It is zero indexed
+It is recommended to create lists that store one type of values
+'''
+
+lista = list() # Using casting
+lista = [] # using square brackets
+
+lista = ['String', 10, 3.1415, True] # Multiple data type
+
+print(lista)
