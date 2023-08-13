f778cbd1e92f9a1eb3b32bf8d0e6201ac865b158
Salomon Chambi <salomonchambi@gmail.com>
2023-08-13 07:00:00 -0500

    feat: access list elements by index

---
+++ fundamentals/2.lists/1-list_index.py
@@ -0,0 +1,12 @@
+'''
+Python lists are zero indexed meaning the first element of the list in index = 0
+We can access list elements using its indexes
+'''
+
+#               0           1       2           3       4                       
+#               -5          -4      -3          -2      -1
+course_list = ['Python', 'Django', 'Flask', 'Ruby', 'Java']
+
+print(course_list[0])  # Print the fisrt element of the list 'Python'
+print(course_list[1])  # Print the second element of the list 'Django'
+print(course_list[-1])  # Print the last element of the list 'Java'
