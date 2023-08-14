2e4ce22f2ef48ba10d4ba804cabc6da582a638ef
Salomon Chambi <salomonchambi@gmail.com>
2023-08-14 07:00:00 -0500

    feat: update list element by index

--- fundamentals/2.lists/1-list_index.py
+++ fundamentals/2.lists/1-list_index.py
@@ -7,6 +7,10 @@ We can access list elements using its indexes
 #               -5          -4      -3          -2      -1
 course_list = ['Python', 'Django', 'Flask', 'Ruby', 'Java']
 
+print(course_list)  # Print original list
 print(course_list[0])  # Print the fisrt element of the list 'Python'
 print(course_list[1])  # Print the second element of the list 'Django'
 print(course_list[-1])  # Print the last element of the list 'Java'
+
+course_list[-1] = 'Rust'
+print(course_list)  # Print list with the element in -1 index updated to 'Rust'
