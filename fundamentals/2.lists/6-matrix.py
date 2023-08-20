35dbd9a214a3e3680b0c844b00c2099e1ffe5084
Salomon Chambi <salomonchambi@gmail.com>
2023-08-20 07:00:00 -0500

    feat: create a matrix using lists

---
+++ fundamentals/2.lists/6-matrix.py
@@ -0,0 +1,11 @@
+'''
+We can create a matrix using nested list (lists whitin lists)
+Remember: lists are zero indexed
+'''
+
+col_a = [10, 20, 30, 40]
+col_b = [30, 40, 50, 60]
+
+matriz = [col_a, col_b] # 2 x 4
+
+print(matriz[1][3])
