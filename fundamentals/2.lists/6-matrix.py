9292afff3391768a6ebde4852da78d9abc40448c
Salomon Chambi <salomonchambi@gmail.com>
2023-08-21 07:00:00 -0500

    feat: create a cube (3D list) using lists (commented out)

--- fundamentals/2.lists/6-matrix.py
+++ fundamentals/2.lists/6-matrix.py
@@ -9,3 +9,17 @@ col_b = [30, 40, 50, 60]
 matriz = [col_a, col_b] # 2 x 4
 
 print(matriz[1][3])
+
+# Create a 3D list (Cube)
+
+# # Define the size of the cube (side length)
+# side_length = 3
+
+# # Create a 3D list to represent the cube
+# cube = [[[0 for _ in range(side_length)] for _ in range(side_length)] for _ in range(side_length)]
+
+# # Display the cube
+# for layer in cube:
+#     for row in layer:
+#         print(row)
+#     print()
