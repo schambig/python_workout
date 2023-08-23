17c56c75981566ec2ffc1f4f6f51502cce96afb5
Salomon Chambi <salomonchambi@gmail.com>
2023-08-23 07:00:00 -0500

    feat: define python tuples

---
+++ fundamentals/3.tuples/0-tuples.py
@@ -0,0 +1,13 @@
+'''
+Inmmutable data type!
+Tuples are zero indexed!
+We can store multiple data types into tuples
+We use parenthesis to use tuples
+(kinda lists but lists are mutable data types)
+'''
+
+#           0       1   2       3       4           5 
+tupla = ('String', 10, 3.1415, True, [1, 2, 3], (4, 5, 6))
+
+print(tupla)
+print(type(tupla))
