1c57c10e23a34925ab5e7099b690417af7053972
Salomon Chambi <salomonchambi@gmail.com>
2023-09-02 07:00:00 -0500

    feat: use the print with sep and end

---
+++ fundamentals/4.strings/2-print.py
@@ -0,0 +1,13 @@
+'''
+Primary function for outputting values to the console.
+Supports options like sep for separator and end for the end character.
+Can redirect output to a file using the file parameter.
+Supports escape sequences for special characters like newline and tab.
+
+REMEMBER!! print() functions just prints to stdout
+'''
+
+name = 'Salomon'
+name2 = 'Luis'
+
+print(name, name2, 'Chambi', sep='*') # we can use different chars as sep
