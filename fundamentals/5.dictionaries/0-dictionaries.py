37470c62e478d5bcbb21efdcc3f89499fe2958a6
Salomon Chambi <salomonchambi@gmail.com>
2023-09-07 07:00:00 -0500

    feat: define dictionaries, get keys, values and items

---
+++ fundamentals/5.dictionaries/0-dictionaries.py
@@ -0,0 +1,34 @@
+'''
+Mutable data type!
+Dictionaries are unordered, mutable, and can contain different data types.
+A dictionary is a built-in data type that is used to store and manage
+collections of key-value pairs.
+It is also known as an associative array, map, or hash table in other
+programming languages.
+They are particularly useful when you need to associate data
+with specific labels or identifiers.
+'''
+
+# We can difine a dictionary in two ways:
+dictionary = {}
+dictionary = dict()
+
+# We can use different data type as key:
+dictionary = {'total': 55, 10: 'Python Course', (1, 2, 3): True}
+# keys:         string       int                 tuple
+
+# Accessing Keys and Values:
+diccionario = {'a': 1, 'b': 2, 'c': 3, 'd': 4}
+
+# keys
+# values
+# items
+
+keys = diccionario.keys() # note that I;m not casting to tuple
+print(keys) # dict_keys(['a', 'b', 'c', 'd']))
+
+values = tuple(diccionario.values())
+print(values) # (1, 2, 3, 4)
+
+items = tuple(diccionario.items())
+print(items) # (('a', 1), ('b', 2), ('c', 3), ('d', 4))
