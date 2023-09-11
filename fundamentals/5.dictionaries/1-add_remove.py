95517bd98f186897ee30b6bdcdd6a169f2c92f23
Salomon Chambi <salomonchambi@gmail.com>
2023-09-11 07:00:00 -0500

    feat: remove key-value pairs with , pop() and clear()

--- fundamentals/5.dictionaries/1-add_remove.py
+++ fundamentals/5.dictionaries/1-add_remove.py
@@ -7,11 +7,19 @@ elements = {}
 
 elements['name'] = 'Salo' # create the key-value pair
 elements[(1, 2, 3)] = 'The key is a tuple'
+elements['course'] = 'Python'
 elements['name'] = 'Kodama' # update the value created in line 8
 print(elements)
+# {'name': 'Kodama', (1, 2, 3): 'The key is a tuple', 'course': 'Python'}
 
-del elements['name'] # remove the key-value pair
+# Remove key-value pairs with `del` keyword and pop() method
+del elements['name'] # {(1, 2, 3): 'The key is a tuple', 'course': 'Python'}
+elements.pop('course') # {(1, 2, 3): 'The key is a tuple'}
 print(elements)
+
+# Remove all key-value pairs in the dictionary
+elements.clear()
+print(elements) # {}
 
 dic = {'a': 1, 'b': 2, 'c':3, 'a': 4} # same key with two values, takes last value
 print(dic) # {'a': 4, 'b': 2, 'c': 3}
