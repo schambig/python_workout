4db24c0e6207c778579a25f0c7e34086f1c0500e
Salomon Chambi <salomonchambi@gmail.com>
2023-09-15 07:00:00 -0500

    feat: more ways that are considered falsy

---
+++ fundamentals/6.loops_conditions/1-false_falsy.py
@@ -0,0 +1,27 @@
+'''
+In Python, the primary way to represent the Boolean value False is by using
+the built-in constant False.
+However, there are other values that are considered "falsy" in a Boolean context,
+meaning they are treated as equivalent to False when used in conditions.
+Here are some of them:
+'''
+
+# The False Constant:
+my_variable = False
+
+# Numeric Zero: Numeric values of 0 (integer or float) are considered falsy.
+my_variable = 0
+my_variable = 0.0
+
+# Empty Sequences:
+# Like empty strings, lists, tuples, and dictionaries are considered falsy.
+empty_string = "" or ''
+empty_list = []
+empty_tuple = ()
+empty_dict = {}
+
+# The None constant is considered falsy.
+my_variable = None
+
+# Expressions that evaluate to False in comparison operations are obviously falsy.
+result = (2 == 3)  # result is False
