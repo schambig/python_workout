a779c923eec78d2db369984e67c0496db0cb50f1
Salomon Chambi <salomonchambi@gmail.com>
2023-09-22 07:00:00 -0500

    feat: define and use ternary operator

--- fundamentals/6.loops_conditions/4-ternary.py
+++ fundamentals/6.loops_conditions/4-ternary.py
@@ -7,10 +7,14 @@ The ternary operator is also known as the conditional expression.
 # General syntax
 # value_if_true if condition else value_if_false
 
-# Basic example
+# Basic examples
 x = 5
 result = "Even" if x % 2 == 0 else "Odd"
 print(result) # Odd
+
+calificacion = 10
+color = 'Verde' if calificacion >= 7 else 'Rojo'
+print(calificacion, color) # 10 Verde
 
 # We can also nest ternary operator
 x = 10
