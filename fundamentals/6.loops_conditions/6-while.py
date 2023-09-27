86d00f59904d85f80cde566dd23dc7d07d8881a5
Salomon Chambi <salomonchambi@gmail.com>
2023-09-27 07:00:00 -0500

    feat: define while, use else keyword with while loop

--- fundamentals/6.loops_conditions/6-while.py
+++ fundamentals/6.loops_conditions/6-while.py
@@ -15,9 +15,10 @@ while count <= 5:
 
 # Get the number of digits in a number using a wile loop and dividing by 10
 number = 1234567890
+num = number # save the original number to print in the message
 counter = 0
 while number >= 1:
     counter += 1
     number /= 10
 else: # use else keyword to execute another block of code when needed
-    print(counter) 
+    print(f'El numero {num} tiene {counter} digitos.') 
