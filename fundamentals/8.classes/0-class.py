c9cd6d5725dfd01fc1ba6e9e135bded522993e1d
Salomon Chambi <salomonchambi@gmail.com>
2023-10-18 07:00:00 -0500

    feat: create and instantiate a simple class

---
+++ fundamentals/8.classes/0-class.py
@@ -0,0 +1,15 @@
+'''
+A class is a blueprint for creating objects.
+Objects are instances of classes, and classes define the properties (attributes)
+and behaviors (methods) that objects created from the class will have.
+Classes provide a way to structure and organize code in an object-oriented programming
+(OOP) paradigm.
+'''
+
+# Simple syntax to create a class
+class User:  # Use CamelCase and singular for the class name
+    pass
+
+# create an instance (object) of the User class
+salo = User()
+print(salo)  # <__main__.User object at 0x7f0c03c838e0>, the object is of User type
