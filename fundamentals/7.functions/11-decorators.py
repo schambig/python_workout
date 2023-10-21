9d96bb4a6957638bf285b4ec031d05b326fc0799
Salomon Chambi <salomonchambi@gmail.com>
2023-10-21 07:00:00 -0500

    feat: explain decorator with coffee analogy

--- fundamentals/7.functions/11-decorators.py
+++ fundamentals/7.functions/11-decorators.py
@@ -52,3 +52,49 @@ sum()
 # Before calling function_b
 # 40
 # After calling function_b
+
+'''
+Analogy: The Coffee Shop Decorator
+    Imagine you're the owner of a coffee shop, and you have different types of coffee
+    that customers can order.
+    Each type of coffee is like a function, and you want to add some extra service or decoration
+    to certain types of coffee.
+    Now, instead of modifying the recipe (function) for each type of coffee,
+    you decide to use decorators.
+'''
+
+# 1.Plain Coffee (Original Function):
+#   Your basic coffee is just plain black coffee.
+def plain_coffee():
+    return "Black Coffee"
+
+# 2.Decorator: Adding Sugar (Decorator Function):
+#   You create a decorator to add sugar to any coffee.
+def add_sugar(coffee_function):
+    def wrapper():
+        return coffee_function() + " with Sugar"
+    return wrapper
+
+# 3.Decorate a Coffee Type (Apply Decorator):
+#   Now, you decorate your plain coffee function with the add_sugar decorator.
+@add_sugar
+def plain_coffee():
+    return "Black Coffee"
+
+# 4.Serve Decorated Coffee (Call Decorated Function):
+#   When a customer orders plain coffee, you serve them the decorated version.
+print(plain_coffee())  # Black Coffee with Sugar
+
+'''
+`plain_coffee` is like a basic function representing black coffee.
+`add_sugar` is a decorator function that takes another function (coffee_function) and returns
+a new function (`wrapper`) that adds sugar to the result of the original function.
+`@add_sugar` is like applying the decorator to the `plain_coffee` function.
+When you call `plain_coffee()`, you're actually calling the decorated version that includes
+the sugar.
+In this analogy, the decorator (`add_sugar`) is a reusable way to enhance the behavior
+of different types of coffee (functions) without modifying their original recipes.
+You can have other decorators (`add_milk`, `add_caramel`) to modify the coffee in different ways,
+and you can mix and match these decorators as needed.
+This helps keep your coffee recipes (functions) clean and modular.
+'''
