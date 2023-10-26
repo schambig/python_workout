6dabc924a980302bdfe3da02824edefad8f9f44c
Salomon Chambi <salomonchambi@gmail.com>
2023-10-26 07:00:00 -0500

    feat: return tip with two decimal rounded

---
+++ basic_challenges/2-caculate_tip.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def calculate_tip(bill_amount, tipPercentage):
+   return round((bill_amount * tipPercentage / 100), 2)
+
+# print(calculate_tip(100, 10))
+# print(calculate_tip(1524.33, 25))
