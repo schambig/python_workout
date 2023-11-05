5f0ab85c56ec8bae1663c9e22e69f51bd7edb45b
Salomon Chambi <salomonchambi@gmail.com>
2023-11-05 11:08:40 -0500

    feat: find the largest palindrome

--- basic_challenges/9-largest_palindrome.py
+++ basic_challenges/9-largest_palindrome.py
@@ -9,17 +9,18 @@ def find_largest_palindrome(words):
       pal = p
   return pal if pal else None
 
-# # Using a conditional and max()
-# def find_largest_palindrome(words):
-#   pals = [w for w in words if w == w[::-1]]
-#   if pals:
-#     pal = max(pals, key=len)
-#     return pal if pal else None
+# Using a conditional and max()
+def find_largest_palindrome2(words):
+  pals = [w for w in words if w == w[::-1]]
+  if pals:
+    pal = max(pals, key=len)
+    return pal if pal else None
 
-# # Shortest version, using max() in a better way  
-# def find_largest_palindrome(words):
-#   pals = [p for p in words if p == p[::-1]]
-#   return max(pals, key=len, default=None)
+# Shortest version, using max() in a better way  
+def find_largest_palindrome3(words):
+  pals = [p for p in words if p == p[::-1]]
+  return max(pals, key=len, default=None)
+
   
-# print(find_largest_palindrome(["racecar", "level", "world", "hello"]))
-# print(find_largest_palindrome(["Hibou", "Python", "xml", "owl"]))
+print(find_largest_palindrome(["racecar", "level", "world", "hello"]))
+print(find_largest_palindrome(["Hibou", "Python", "xml", "owl"]))
