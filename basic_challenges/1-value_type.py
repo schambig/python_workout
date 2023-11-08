ca148cf9e2c550a869f5f327496a50378a997404
Salomon Chambi <salomonchambi@gmail.com>
2023-11-08 07:00:00 -0500

    feat: print the type of the value

--- basic_challenges/1-value_type.py
+++ basic_challenges/1-value_type.py
@@ -3,6 +3,7 @@
 def found_type(value):
     return type(value)
 
-# print(found_type(1))
-# print(found_type('Salo'))
-# print(found_type(True))
+
+print(found_type(1))
+print(found_type('Salo'))
+print(found_type(True))
