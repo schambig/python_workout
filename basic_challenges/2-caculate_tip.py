66853b9ebed8a0f00b85bfa14a7a6d246c59a573
Salomon Chambi <salomonchambi@gmail.com>
2023-11-09 07:00:00 -0500

    feat: calculate the tip

--- basic_challenges/2-caculate_tip.py
+++ basic_challenges/2-caculate_tip.py
@@ -3,5 +3,6 @@
 def calculate_tip(bill_amount, tipPercentage):
    return round((bill_amount * tipPercentage / 100), 2)
 
-# print(calculate_tip(100, 10))
-# print(calculate_tip(1524.33, 25))
+
+print(calculate_tip(100, 10))
+print(calculate_tip(1524.33, 25))
