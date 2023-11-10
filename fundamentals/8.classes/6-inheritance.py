13a1c00ca13ea1692c4061ff567a0ccb8e5df33a
Salomon Chambi <salomonchambi@gmail.com>
2023-11-10 07:00:00 -0500

    feat: define multiple inheritance

--- fundamentals/8.classes/6-inheritance.py
+++ fundamentals/8.classes/6-inheritance.py
@@ -17,3 +17,81 @@ Here's a brief explanation of how inheritance works in Python:
   - If a method is defined in both the parent and the child class with the same name,
   the method in the child class will override the one in the parent class. This is known as method overriding.
 '''
+
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self):
+        pass  # Abstract method, to be overridden in subclasses
+
+class Dog(Animal):
+    def speak(self):
+        return "Woof!"
+
+class Cat(Animal):
+    def speak(self):
+        return "Meow!"
+
+# Creating instances of the child classes
+dog_instance = Dog("Pequeña")
+cat_instance = Cat("Garfilito")
+
+# Using the inherited methods
+print(dog_instance.speak())  # Woof!
+print(cat_instance.speak())  # Meow!
+
+'''
+In this example, the Dog and Cat classes inherit from the Animal class.
+They both have a speak method, which overrides the abstract speak method in the Animal class.
+
+Inheritance in Python provides a way to create a hierarchy of classes, promoting code reuse,
+and making it easier to structure and organize your code.
+'''
+
+
+'''MULTIPLE INHERITANCE
+The syntax for multiple inheritance is as follows:
+    class ParentClass1:
+        # Attributes and methods of the first parent class
+
+    class ParentClass2:
+        # Attributes and methods of the second parent class
+
+    class ChildClass(ParentClass1, ParentClass2):
+        # Attributes and methods of the child class
+'''
+
+class Animal:
+    def make_sound(self):
+        pass  # Abstract method, to be overridden in subclasses
+
+class Mammal:
+    def feed_milk(self):
+        return "Feeding milk"
+
+class Dog(Animal, Mammal):
+    def make_sound(self):
+        return "Woof!"
+
+class Cat(Animal, Mammal):
+    def make_sound(self):
+        return "Meow!"
+
+# Creating instances of the child classes
+dog_instance = Dog()
+cat_instance = Cat()
+
+# Using the inherited methods
+print(dog_instance.make_sound())  # Woof!
+print(dog_instance.feed_milk())  # Feeding milk
+
+print(cat_instance.make_sound())  # Meow!
+print(cat_instance.feed_milk())  # OFeeding milk
+
+'''
+While multiple inheritance can be a powerful feature, it can also lead to complexities
+and challenges, such as the "diamond problem".
+Exercise caution and consider alternatives, such as composition,
+when designing your class hierarchy to maintain code readability and avoid potential issues.
+'''
