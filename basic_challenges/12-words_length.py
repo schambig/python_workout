aa9ddfff5634a27619ccb9f377747cc38a8ffab2
Salomon Chambi <salomonchambi@gmail.com>
2023-11-11 07:00:00 -0500

    feat: count words by length

---
+++ basic_challenges/12-words_length.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+# Short version using list and dictionary comprehensions.
+def count_words_by_length(words):
+  keys = [len(w) for w in words]
+  return {k: keys.count(k) for k in keys}
+
+# Large version using loop and conditional.
+def count_words_by_length(words):
+  dic = {}
+  for w in words:
+    key = len(w)
+    if key not in dic:
+      dic[key] = 1
+    else:
+      dic[key] += 1
+  return dic
+
+
+print(count_words_by_length([
+  "apple",
+  "banana",
+  "orange",
+  "grapefruit",
+  "pear",
+  "kiwi"
+]))
+
+print(count_words_by_length([
+  "apple",
+  "banana",
+  "orange"
+]))
