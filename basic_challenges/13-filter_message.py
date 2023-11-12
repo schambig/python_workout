6e3484f03a585426b363173216211af0a105086c
Salomon Chambi <salomonchambi@gmail.com>
2023-11-12 18:45:20 -0500

    feat: filter messages by user

---
+++ basic_challenges/13-filter_message.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+# Using lambda funtion.
+def filter_user_messages(messages, user):
+  return  list(filter(lambda x: x['sender'] == user, messages))
+
+# Using list comprehension
+def filter_user_messages2(messages, user):
+  return [m for m in messages if m['sender'] == user]
+
+# Using loop and conditional.
+def filter_user_messages3(messages, user):
+  lis = []
+  for m in messages:
+    if m['sender'] == user:
+      lis.append(m)
+  return lis
+
+
+messages = [
+  {'sender': 'Alice', 'content': 'Hola, ¿cómo estás?'},
+  {'sender': 'Bob', 'content': '¡Bien, gracias!'},
+  {'sender': 'Alice', 'content': '¿Quieres tomar un café?'},
+  {'sender': 'Charlie', 'content': 'Hola a todos.'},
+  {'sender': 'Alice', 'content': 'Nos vemos luego.'}
+]
+
+user = 'Alice'
+print(filter_user_messages(messages, user))
+
+user = 'Charlie'
+print(filter_user_messages(messages, user))
+
+user = 'Bob'
+print(filter_user_messages(messages, user))
