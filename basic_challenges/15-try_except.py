2c80686394b88985785096c10e686dd3c853845e
Salomon Chambi <salomonchambi@gmail.com>
2023-11-15 07:00:00 -0500

    feat: handle errors with try-except statement

--- basic_challenges/15-try_except.py
+++ basic_challenges/15-try_except.py
@@ -4,10 +4,10 @@
 def calculate_average(numbers):
     try:
         if not numbers:
-            raise ValueError('La lista está vacía')
+            raise ValueError('The list is empty.')
         return sum(numbers) / len(numbers)
     except TypeError:
-        raise TypeError('La lista contiene elementos no numéricos')
+        raise TypeError('The list contains non-numeric elements.')
   
 # using two except keywords:
 def calculate_average2(numbers):
@@ -15,17 +15,17 @@ def calculate_average2(numbers):
         # result = sum(numbers) / len(numbers)
         return sum(numbers) / len(numbers)
     except ZeroDivisionError:  # consider 0 elements in the list -> 0/0
-        raise ValueError("La lista está vacía")
+        raise ValueError('The list is empty.')
     except TypeError:
-        raise TypeError("La lista contiene elementos no numéricos")
+        raise TypeError('The list contains non-numeric elements.')
     # else:
     #     return result
   
 
-print(calculate_average2([1, 2, 3, 4, 5]))
+print(calculate_average([1, 2, 3, 4, 5]))
 
-print(calculate_average2([10, 20, 30, 40, 50]))
+print(calculate_average([10, 20, 30, 40, 50]))
 
-print(calculate_average2([]))
+print(calculate_average([]))
 
-print(calculate_average2([1, 2, '3', 4, 5]))
+print(calculate_average([1, 2, '3', 4, 5]))
