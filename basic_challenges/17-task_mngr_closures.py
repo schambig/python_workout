2d778814cf4bdf6707289232f53a4f399f62ad0f
Salomon Chambi <salomonchambi@gmail.com>
2023-11-20 07:00:00 -0500

    feat: add tasks creating a new dict, use get() and import pprint

--- basic_challenges/17-task_mngr_closures.py
+++ basic_challenges/17-task_mngr_closures.py
@@ -1,22 +1,37 @@
 #!/usr/bin/python3
+from pprint import pprint # pretty-print module
 
-# update dictionary with `update()` function
+
 def createTaskPlanner():
-  tasks = []
-  def addTask(task):
-    task.update({
-      'id': task['id'],
-      'name': task['name'],
-      'priority': task['priority'],
-      'tags': task['tags'],
-      'completed': False, # create a key-value element
-    })
-    tasks.append(task)
+    tasks = []
 
-  return {
-    'addTask': addTask,
-    'tasks': tasks,
-  }
+    def addTask(task):
+        # # update dictionary with `update()` function
+        # task.update({
+        #     'id': task['id'],
+        #     'name': task['name'],
+        #     'priority': task['priority'],
+        #     'tags': task['tags'],
+        #     'completed': False, # create a key-value element
+        # })
+        # tasks.append(task)
+
+        # create a new dictionary with new values and append to the list
+        update_task = {
+            'id': task['id'],
+            'name': task['name'],
+            'priority': task['priority'],
+            'tags': task['tags'],
+            # 'completed': True if task['completed'] == True else False, # look for completed key, throws a key error if 'complete' key is not present
+            # 'completed': task['completed'] if 'completed' in task else False, # same result as below
+            'completed': task.get('completed', False),
+        }
+        tasks.append(update_task)
+
+    return {
+      'addTask': addTask,
+      'tasks': tasks,
+    }
 
 # create a closure
 planner = createTaskPlanner()
@@ -24,9 +39,17 @@ planner = createTaskPlanner()
 # use the closure to add a task
 planner['addTask']({
     'id': 1,
-    'name': 'Comprar leche',
+    'name': 'Buy milk',
     'priority': 1,
     'tags': ['shopping', 'home']
 })
 
-print(planner['tasks'])
+planner['addTask']({
+    'id': 2,
+    'name': 'Test2',
+    'priority': 2,
+    'tags': ['growing', 'jumping'],
+    'completed': True,
+})
+
+pprint(planner['tasks'])
