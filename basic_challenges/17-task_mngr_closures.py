582571453e972b37e382142d00c352c6a30600e4
Salomon Chambi <salomonchambi@gmail.com>
2023-11-21 07:00:00 -0500

    feat: add tasks using get(), add get() method definition

--- basic_challenges/17-task_mngr_closures.py
+++ basic_challenges/17-task_mngr_closures.py
@@ -12,21 +12,25 @@ def createTaskPlanner():
         #     'name': task['name'],
         #     'priority': task['priority'],
         #     'tags': task['tags'],
-        #     'completed': False, # create a key-value element
+        #     'completed': False, # create a key-value element with False even if you pass True
         # })
         # tasks.append(task)
 
-        # create a new dictionary with new values and append to the list
-        update_task = {
-            'id': task['id'],
-            'name': task['name'],
-            'priority': task['priority'],
-            'tags': task['tags'],
-            # 'completed': True if task['completed'] == True else False, # look for completed key, throws a key error if 'complete' key is not present
-            # 'completed': task['completed'] if 'completed' in task else False, # same result as below
-            'completed': task.get('completed', False),
-        }
-        tasks.append(update_task)
+        # # create a new dictionary with new values and append to the list
+        # update_task = {
+        #     'id': task['id'],
+        #     'name': task['name'],
+        #     'priority': task['priority'],
+        #     'tags': task['tags'],
+        #     # 'completed': True if task['completed'] == True else False, # look for completed key, throws a key error if 'complete' key is not present
+        #     # 'completed': task['completed'] if 'completed' in task else False, # same result as below
+        #     'completed': task.get('completed', False),  # TAKE A LOOK AT THE END, get() function definition
+        # }
+        # tasks.append(update_task)
+
+        # since we are passing the dict we just need to set/create the 'completed' key 
+        task['completed'] = task.get('completed', False)   # TAKE A LOOK AT THE END, get() function definition
+        tasks.append(task)
 
     return {
       'addTask': addTask,
@@ -52,4 +56,41 @@ planner['addTask']({
     'completed': True,
 })
 
+planner['addTask']({
+    'id': 3,
+    'name': 'Test3',
+    'priority': 3,
+    'tags': ['coding', 'napping']
+})
+
 pprint(planner['tasks'])
+
+
+'''get() method:
+
+`get()` is a dictionary method in Python that is used to retrieve the value of a specified key.
+It allows you to access the value associated with a key in a dictionary while providing
+a default value if the key is not found.
+
+The syntax for get() is as follows:
+    value = dictionary.get(key, default_value)
+
+Here's a more detailed breakdown of how it works in the context of the closure `addTask()` (see at the top):
+    update_task = {
+        'id': task['id'],
+        'name': task['name'],
+        'priority': task['priority'],
+        'tags': task['tags'],
+        'completed': task.get('completed', False),
+    }
+
+  - task.get('completed', False): This line attempts to retrieve the value associated with
+    the key 'completed' from the task dictionary.
+    If the key exists in the task dictionary, it returns the corresponding value.
+    If the key is not found, it returns the default value provided as the second argument
+    (in this case, False).
+  - In this specific case, it means that if the input `task` dictionary has a key 'completed',
+    its value will be used for the 'completed' key in the `update_task` dictionary.
+    If the 'completed' key is not present in the input task,
+    the default value False will be used.
+'''
