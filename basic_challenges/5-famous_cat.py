d1b28a0d07aee9af29f2daee959f7e3b85798a25
Salomon Chambi <salomonchambi@gmail.com>
2023-12-05 07:00:00 -0500

    feat: find the most famous cat

--- basic_challenges/5-famous_cat.py
+++ basic_challenges/5-famous_cat.py
@@ -13,43 +13,43 @@ def find_famous_cat(cats):
       cat.append(c['name'])
   return cat
 
-# # Larger version
-# def find_famous_cat(cats):
-#   fans = []
-#   for d in cats:
-#     fans.append(sum(d['followers']))
-#   mayor = fans[0]
-#   for n in fans:
-#     if n > mayor:
-#       mayor = n
-#   cat = []  
-#   for c in cats:
-#     if sum(c['followers']) == mayor:
-#       cat.append(c['name'])
-#   return cat
+# Larger version
+def find_famous_cat2(cats):
+  fans = []
+  for d in cats:
+    fans.append(sum(d['followers']))
+  mayor = fans[0]
+  for n in fans:
+    if n > mayor:
+      mayor = n
+  cat = []  
+  for c in cats:
+    if sum(c['followers']) == mayor:
+      cat.append(c['name'])
+  return cat
 
-# print(find_famous_cat([
-#   {
-#     "name": "Luna",
-#     "followers": [500, 200, 300]
-#   },
-#   {
-#     "name": "Michi",
-#     "followers": [100, 300]
-#   },
-# ]))
+print(find_famous_cat([
+  {
+    "name": "Luna",
+    "followers": [500, 200, 300]
+  },
+  {
+    "name": "Michi",
+    "followers": [100, 300]
+  },
+]))
 
-# print(find_famous_cat([
-#   {
-#     "name": "Mimi",
-#     "followers": [320, 120, 70]
-#   },
-#   {
-#     "name": "Milo",
-#     "followers": [400, 300, 100, 200]
-#   },
-#   {
-#     "name": "Gizmo",
-#     "followers": [250, 750]
-#   }
-# ]))
+print(find_famous_cat([
+  {
+    "name": "Mimi",
+    "followers": [320, 120, 70]
+  },
+  {
+    "name": "Milo",
+    "followers": [400, 300, 100, 200]
+  },
+  {
+    "name": "Gizmo",
+    "followers": [250, 750]
+  }
+]))
