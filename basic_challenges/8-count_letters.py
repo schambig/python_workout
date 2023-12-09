898a0b9de5a2d0c35755873fc936e065085222c4
Salomon Chambi <salomonchambi@gmail.com>
2023-12-09 07:00:00 -0500

    feat: count the number of ocurrences of each letter

--- basic_challenges/8-count_letters.py
+++ basic_challenges/8-count_letters.py
@@ -1,23 +1,29 @@
 #!/usr/bin/python3
 
-# If the .count() method is used,
+# short version
+# If the count() method is used,
 # a count will be made on the string for each character traversed in the string's for loop,
 # resulting in an algorithm with a complexity of O(n²).
 def count_letters(phrase):
     return {char: phrase.count(char) for char in phrase}
 
-# # If we use the dictionary.get() method,
+# long version
+# # If we use the get() method,
 # # the value of each character will be summed in a single total iteration of the string,
 # # resulting in a complexity of O(n).
-# def count_letters(phrase):
-#   dic = {}
-#   for i in phrase:
-#     dic[i] = dic.get(i, 0) + 1
-#   return dic
+def count_letters(phrase):
+  dic = {}
+  for i in phrase:
+    dic[i] = dic.get(i, 0) + 1
+  return dic
 
-# Performance is better with the second function.
-# While in this very simple example there is no noticeable difference,
-# it will be useful to keep in mind when iterating and creating much larger and more complex elements.
+'''
+Performance is better with the second function.
+While in this very simple example there is no noticeable difference,
+it will be useful to keep in mind when iterating and creating much larger and more complex
+elements.
+'''
 
-# print(count_letters("Hola mundo"))
-# print(count_letters("Hello world"))
+
+print(count_letters("Hola mundo"))
+print(count_letters("Hello world"))
