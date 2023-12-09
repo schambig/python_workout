61ffa16cbb0cc8b872f4dda1691e788970f8a98d
Salomon Chambi <salomonchambi@gmail.com>
2023-12-09 07:00:00 -0500

    feat: count the number of ocurrences of each letter

--- basic_challenges/8-count_letters.py
+++ basic_challenges/8-count_letters.py
@@ -18,6 +18,29 @@ def count_letters(phrase):
   return dic
 
 '''
+line 17 translates to:
+
+    # Assume i = 'r'
+    if 'r' in dic:
+        dic['r'] = dic['r'] + 1  # Increment the count for 'r'
+    else:
+        dic['r'] = 0 + 1         # Initialize the count for 'r' to 0 and then increment it
+
+Now, let's consider this process for the letter 'r' in the context of a loop.
+If the letter 'r' is encountered multiple times in the loop,
+the count will be incremented each time:
+    1.First encounter (assuming dic is initially empty):
+        - 'r' is not in dic, so it initializes the count to 0 and increments it by 1.
+        - Result: dic['r'] = 1
+    2.Second encounter (assuming dic is now {'r': 1}):
+        - 'r' is in dic, so it increments the count by 1.
+        - Result: dic['r'] = 2
+    3.Third encounter (assuming dic is now {'r': 2}):
+        - 'r' is in dic, so it increments the count by 1.
+        - Result: dic['r'] = 3
+''' 
+
+'''
 Performance is better with the second function.
 While in this very simple example there is no noticeable difference,
 it will be useful to keep in mind when iterating and creating much larger and more complex
