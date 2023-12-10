78d36d9e165b8acdd9fd36230f547d61c4eeaade
Salomon Chambi <salomonchambi@gmail.com>
2023-12-10 00:02:54 -0500

    feat: create Product parent class

---
+++ basic_challenges/product.py
@@ -0,0 +1,12 @@
+#!/usr.bin/python3
+
+class Product:
+    '''This Class is used by 19-shopping_cart.py module'''
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+
+    def addToCart(self):
+        raise NotImplementedError("This method will be overriden by the children classes")
