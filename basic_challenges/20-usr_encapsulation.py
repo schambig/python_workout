565689f7af95cacf2035da2443c5cc5413be43b3
Salomon Chambi <salomonchambi@gmail.com>
2023-12-11 10:43:05 -0500

    feat: use name mangling to access private variables

--- basic_challenges/20-usr_encapsulation.py
+++ basic_challenges/20-usr_encapsulation.py
@@ -2,13 +2,13 @@
 
 class User:
     def __init__(self, name, age):
-        self.__name = name
-        self.__age = age
-        self._friends = []
-        self._messages = []
+        self.__name = name  # private variable, use __
+        self.__age = age  # private variable
+        self._friends = []  # protected variable, use _
+        self._messages = []  # protected variable
 
 
 # # Accessing the private variable using name mangling
 usr = User('Harvey', 30)
-print(usr._User__name)
-print(usr._User__age)
+print(usr._User__name)  # Harvey
+print(usr._User__age)  # 30
