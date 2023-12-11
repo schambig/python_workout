dcd5daa81d32460e9868f9a743796cd8ca66bb06
Salomon Chambi <salomonchambi@gmail.com>
2023-12-10 19:03:09 -0500

    feat: create Cart class, implement its methods

--- basic_challenges/19-shopping_cart.py
+++ basic_challenges/19-shopping_cart.py
@@ -12,7 +12,40 @@ class Service(Product):
         return f'Adding service {self.name} to the cart' 
 
 
+class Cart:
+    def __init__(self):
+        self.shopping_cart = []
+    
+
+    def addProduct(self, product):
+        self.shopping_cart.append(product)
+        return product.addToCart()
+
+
+    def deleteProduct(self, product):
+        self.shopping_cart.remove(product)
+        return f'Deleting {product.name} from shopping cart'
+
+
+    def caculateTotal(self):
+        return sum((product.price * product.quantity) for product in self.shopping_cart)
+
+
+    def getProducts(self):
+        return [(product.name, product.price, product.quantity) \
+                for product in self.shopping_cart]
+
+
 book = Article('Book', 100, 2)
-print(book.addToCart())
+# print(book.addToCart())
 course = Service('Course', 150, 1)
-print(course.addToCart())
+# print(course.addToCart())
+
+cart = Cart()
+print(cart.addProduct(book))
+print(cart.addProduct(course))
+print(cart.getProducts())
+# print(cart.deleteProduct(book))
+print(cart.deleteProduct(course))
+# print(cart.getProducts())
+print(cart.caculateTotal())
