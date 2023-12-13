a160c13bfdc09a3760b58ec4a1b20449455ed41a
Salomon Chambi <salomonchambi@gmail.com>
2023-12-13 10:09:02 -0500

    feat: create Pay class and its method make_pay

---
+++ basic_challenges/pay.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+class Pay:
+    def __init__(self):
+        pass
+
+
+    def make_pay(self, quantity):
+        return {
+            'realized': True,
+            'quantity': quantity,  # use the parameter passed to the method, not self.quantity
+        }                          # bacause there's no such attribute defined in the Pay call
