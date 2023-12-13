af6062d9bacafd3e74ea6698b58bf58d3f2925b3
Salomon Chambi <salomonchambi@gmail.com>
2023-12-13 11:12:21 -0500

    feat: receive payment method, amount and call make_pay function

--- basic_challenges/22-payment_polimorphism.py
+++ basic_challenges/22-payment_polimorphism.py
@@ -6,7 +6,7 @@ from cash import Cash
 
 
 '''
-This module works in conjunction with `pay.py`, `paypal.py`, `card.py` modules
+This module works in conjunction with `pay.py`, `paypal.py`, `card.py` and `cash.py` modules
 '''
 
 
@@ -16,11 +16,20 @@ def process_pay(payment_method, amount):
 
 paypal = PayPal('email@example.com')
 pprint(process_pay(paypal, 404))
+# Output:
+# {'email': 'email@example.com',
+#  'platform': 'PayPal',
+#  'quantity': 404,
+#  'realized': True}
 
 
 card = Card('4444333322221111')
 pprint(process_pay(card, 500))
+# Output:
+# {'last_card_numbers': '1111', 'quantity': 500, 'realized': True}
 
 
 cash = Cash()
 pprint(process_pay(cash, 202))
+# Output:
+# {'quantity': 202, 'realized': True}
