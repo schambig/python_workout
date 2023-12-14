753cbdcf4f08183c5a6aff40699491a302da8dfd
Salomon Chambi <salomonchambi@gmail.com>
2023-12-14 13:19:58 -0500

    feat: delete first item, return it and reduce length

--- basic_challenges/23-my_dictionary.py
+++ basic_challenges/23-my_dictionary.py
@@ -18,6 +18,15 @@ class MyDict:
         del self.data[self.length - 1]
         self.length -= 1
         return last_item
+    
+
+    def shift(self):
+        first_item = self.data[0]
+        for item in range(1, self.length):
+            self.data[item - 1] = self.data[item]
+        del self.data[self.length -1]
+        self.length -= 1
+        return first_item
 
 
 dic = MyDict()
@@ -31,3 +40,6 @@ pprint(dic.length)  # 4
 dic.pop()
 pprint(dic.data)  # {0: 'S', 1: 'a', 2: 'l'}
 pprint(dic.length)  # 3
+
+dic.shift()
+pprint(dic.data)  # {0: 'a', 1: 'l'}
