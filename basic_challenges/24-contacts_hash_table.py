37a7d453f3225a5ad891be8629a12f7d816ef193
Salomon Chambi <salomonchambi@gmail.com>
2023-12-15 07:00:00 -0500

    feat: use hash() built-in function

--- basic_challenges/24-contacts_hash_table.py
+++ basic_challenges/24-contacts_hash_table.py
@@ -5,8 +5,14 @@ class ContactList:
         self.size = size
         self.contacts_list = [[] for _ in range(self.size)]
 
+    
+    # hash() function is a built-in function that returns the hash value of an object.
+    def hash(self, key):
+        return hash(key) % self.size
+
 
 conts_list = ContactList(5)
 print(conts_list.size)
 print(conts_list.contacts_list)
-        
+
+print(conts_list.hash('Test'))
