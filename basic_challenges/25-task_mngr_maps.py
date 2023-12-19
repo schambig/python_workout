1b0f2d8de0fa3ee375c4d14f5fb754cdcf886613
Salomon Chambi <salomonchambi@gmail.com>
2023-12-19 11:26:56 -0500

    feat: add comments and output for add_task method

--- basic_challenges/25-task_mngr_maps.py
+++ basic_challenges/25-task_mngr_maps.py
@@ -8,19 +8,26 @@ class TaskManager:
 
 
     def add_task(self, task, tags):
+        # Convert the task name to lowercase for case-insensitive comparison
         task = task.lower()
+        # Convert the tags to a set to ensure uniqueness
         tags = set(tags)
+        # Check if the task already exists in the dictionary
         if task in self.tasks:
+             # If the task exists, update the set of tags for that task
             self.tasks[task].update(tags)
         else:
+            # If the task does not exist, create a new entry with the given tags
             self.tasks[task] = tags
 
 
 my_task_manager = TaskManager()
-print(my_task_manager.tasks)
+print(my_task_manager.tasks)  # {}
 
 
 my_task_manager.add_task('Buy Milk', ['shopping', 'urgent'])
 my_task_manager.add_task('Walk the Dog', ['pets'])
 my_task_manager.add_task('Workout', ['health'])
 print(my_task_manager.tasks)
+# Output
+# {'buy milk': {'shopping', 'urgent'}, 'walk the dog': {'pets'}, 'workout': {'health'}}
