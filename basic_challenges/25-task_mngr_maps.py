0832c34c0574c98d260a1229def7916dcf0d8f28
Salomon Chambi <salomonchambi@gmail.com>
2023-12-19 11:23:24 -0500

    feat: add task to tasks map

--- basic_challenges/25-task_mngr_maps.py
+++ basic_challenges/25-task_mngr_maps.py
@@ -1,9 +1,26 @@
 #!/usr/bin/python3
+from pprint import pprint
+
 
 class TaskManager:
-  def __init__(self):
-    self.tasks = {}
+    def __init__(self):
+        self.tasks = {}
+
+
+    def add_task(self, task, tags):
+        task = task.lower()
+        tags = set(tags)
+        if task in self.tasks:
+            self.tasks[task].update(tags)
+        else:
+            self.tasks[task] = tags
 
 
 my_task_manager = TaskManager()
+print(my_task_manager.tasks)
+
+
+my_task_manager.add_task('Buy Milk', ['shopping', 'urgent'])
+my_task_manager.add_task('Walk the Dog', ['pets'])
+my_task_manager.add_task('Workout', ['health'])
 print(my_task_manager.tasks)
