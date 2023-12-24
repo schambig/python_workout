76e9368e83594609c0ae84b385267853952dfe6b
Salomon Chambi <salomonchambi@gmail.com>
2023-12-24 09:00:00 -0500

    feat: add new song to the top of the playlist (stack)

--- basic_challenges/27-playlist_stack.py
+++ basic_challenges/27-playlist_stack.py
@@ -10,3 +10,14 @@ class Playlist:
         self.top = None
         self.buttom = None
         self.length = 0
+
+
+    def add_song(self, song):
+        new_song = PlaylistNode(song)
+        if self.length == 0:
+            self.top = new_song
+            self.buttom = new_song
+        else:
+            new_song.next = self.top
+            self.top = new_song
+        self.length += 1
