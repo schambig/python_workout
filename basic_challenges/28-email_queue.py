32d16a71db605561dae4c254879c835c264f9b87
Salomon Chambi <salomonchambi@gmail.com>
2023-12-30 09:00:00 -0500

    feat: define class methods

--- basic_challenges/28-email_queue.py
+++ basic_challenges/28-email_queue.py
@@ -7,3 +7,22 @@ class Queue:
         self.first = None
         self.last = None
         self.length = 0
+
+    def enqueue(self, from_email, to, body, subject):
+        pass
+
+
+    def dequeue(self):
+        pass
+
+
+    def peek(self):
+        pass
+
+
+    def is_empty(self):
+        pass
+
+
+    def size(self):
+        pass
