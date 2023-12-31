d23807af9c8ec0c33e83f71679582ab83f095339
Salomon Chambi <salomonchambi@gmail.com>
2023-12-31 09:00:00 -0500

    feat: implement enqueue method

--- basic_challenges/28-email_queue.py
+++ basic_challenges/28-email_queue.py
@@ -2,14 +2,25 @@
 from mail import Mail
 
 
+''' This module works in conjunction with `mail.py` module '''
+
+
 class Queue:
     def __init__(self):
         self.first = None
         self.last = None
         self.length = 0
 
+
     def enqueue(self, from_email, to, body, subject):
-        pass
+        new_node = Mail(from_email, to, body, subject)
+        if self.length == 0:
+            self.first = new_node
+            self.last = new_node
+        else:
+            self.last.next = new_node
+            self.last = new_node
+        self.length += 1
 
 
     def dequeue(self):
@@ -26,3 +37,13 @@ class Queue:
 
     def size(self):
         pass
+
+
+email_queue = Queue()
+email_queue.enqueue(
+    'salo@example.me',
+    'support@company.com',
+    'I can\'t access my account',
+    'Signinig in issue'
+)
+print(email_queue.__dict__)
